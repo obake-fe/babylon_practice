a528a84882e8fff2a86a74e5f1d0b74e83a269a7
obake-fe <daichismash@gmail.com>
2025-01-25 21:35:47 +0900

    feat: 村を作成

--- src/pages/Village.tsx
+++ src/pages/Village.tsx
@@ -4,6 +4,7 @@ import {
   Color3,
   Engine,
   HemisphericLight,
+  Mesh,
   MeshBuilder,
   Scene,
   Sound,
@@ -31,42 +32,61 @@ export const Village = () => {
     groundMat.diffuseColor = new Color3(0, 1, 0);
 
     const ground = MeshBuilder.CreateGround("ground", {
-      width: 10,
-      height: 10,
+      width: 15,
+      height: 16,
     });
     ground.material = groundMat;
+
+    return ground;
   };
 
   /*
    * 箱を作成
    */
-  const buildBox = () => {
+  const buildBox = (width: number) => {
     // テクスチャ
     const boxMat = new StandardMaterial("boxMat");
-    boxMat.diffuseTexture = new Texture(
-      "https://assets.babylonjs.com/environments/cubehouse.png",
-    );
+    if (width == 2) {
+      boxMat.diffuseTexture = new Texture(
+        "https://assets.babylonjs.com/environments/semihouse.png",
+      );
+    } else {
+      boxMat.diffuseTexture = new Texture(
+        "https://assets.babylonjs.com/environments/cubehouse.png",
+      );
+    }
 
     // それぞれの面に違った画像をあてるためのオプションパラメータ
     const faceUV = [];
-    faceUV[0] = new Vector4(0.5, 0.0, 0.75, 1.0); //rear face
-    faceUV[1] = new Vector4(0.0, 0.0, 0.25, 1.0); //front face
-    faceUV[2] = new Vector4(0.25, 0, 0.5, 1.0); //right side
-    faceUV[3] = new Vector4(0.75, 0, 1.0, 1.0); //left side
+    if (width == 2) {
+      faceUV[0] = new Vector4(0.6, 0.0, 1.0, 1.0); //rear face
+      faceUV[1] = new Vector4(0.0, 0.0, 0.4, 1.0); //front face
+      faceUV[2] = new Vector4(0.4, 0, 0.6, 1.0); //right side
+      faceUV[3] = new Vector4(0.4, 0, 0.6, 1.0); //left side
+    } else {
+      faceUV[0] = new Vector4(0.5, 0.0, 0.75, 1.0); //rear face
+      faceUV[1] = new Vector4(0.0, 0.0, 0.25, 1.0); //front face
+      faceUV[2] = new Vector4(0.25, 0, 0.5, 1.0); //right side
+      faceUV[3] = new Vector4(0.75, 0, 1.0, 1.0); //left side
+    }
     // top 4 and bottom 5 not seen so not set
 
     /**** World Objects *****/
-    const box = MeshBuilder.CreateBox("box", { faceUV: faceUV, wrap: true });
+    const box = MeshBuilder.CreateBox("box", {
+      width,
+      faceUV: faceUV,
+      wrap: true,
+    });
     box.material = boxMat;
     box.position.y = 0.5;
 
-    // return box;
+    return box;
   };
 
   /*
    * 屋根を作成
    */
-  const buildRoof = () => {
+  const buildRoof = (width: number) => {
     // テクスチャ
     const roofMat = new StandardMaterial("roofMat");
     roofMat.diffuseTexture = new Texture(
@@ -80,20 +100,74 @@ export const Village = () => {
     });
     roof.material = roofMat;
     roof.scaling.x = 0.75;
+    roof.scaling.y = width;
     roof.rotation.z = Math.PI / 2;
     roof.position.y = 1.22;
 
-    // return roof;
+    return roof;
   };
 
-  const onSceneReady = useCallback(async (scene: Scene) => {
+  const buildHouse = (width: number) => {
+    const box = buildBox(width);
+    const roof = buildRoof(width);
+
+    return Mesh.MergeMeshes([box, roof], true, false, undefined, false, true);
+  };
+
+  const buildDwellings = () => {
+    buildGround();
+
+    const detached_house = buildHouse(1);
+    detached_house!.rotation.y = -Math.PI / 16;
+    detached_house!.position.x = -6.8;
+    detached_house!.position.z = 2.5;
+
+    const semi_house = buildHouse(2);
+    semi_house!.rotation.y = -Math.PI / 16;
+    semi_house!.position.x = -4.5;
+    semi_house!.position.z = 3;
+
+    const places = []; //each entry is an array [house type, rotation, x, z]
+    places.push([1, -Math.PI / 16, -6.8, 2.5]);
+    places.push([2, -Math.PI / 16, -4.5, 3]);
+    places.push([2, -Math.PI / 16, -1.5, 4]);
+    places.push([2, -Math.PI / 3, 1.5, 6]);
+    places.push([2, (15 * Math.PI) / 16, -6.4, -1.5]);
+    places.push([1, (15 * Math.PI) / 16, -4.1, -1]);
+    places.push([2, (15 * Math.PI) / 16, -2.1, -0.5]);
+    places.push([1, (5 * Math.PI) / 4, 0, -1]);
+    places.push([1, Math.PI + Math.PI / 2.5, 0.5, -3]);
+    places.push([2, Math.PI + Math.PI / 2.1, 0.75, -5]);
+    places.push([1, Math.PI + Math.PI / 2.25, 0.75, -7]);
+    places.push([2, Math.PI / 1.9, 4.75, -1]);
+    places.push([1, Math.PI / 1.95, 4.5, -3]);
+    places.push([2, Math.PI / 1.9, 4.75, -5]);
+    places.push([1, Math.PI / 1.9, 4.75, -7]);
+    places.push([2, -Math.PI / 3, 5.25, 2]);
+    places.push([1, -Math.PI / 3, 6, 4]);
+
+    //Create instances from the first two that were built
+    const houses = [];
+    for (let i = 0; i < places.length; i++) {
+      if (places[i][0] === 1) {
+        houses[i] = detached_house!.createInstance("house" + i);
+      } else {
+        houses[i] = semi_house!.createInstance("house" + i);
+      }
+      houses[i].rotation.y = places[i][1];
+      houses[i].position.x = places[i][2];
+      houses[i].position.z = places[i][3];
+    }
+  };
+
+  const onSceneReady = useCallback((scene: Scene) => {
     /**** Set camera and light *****/
-    // カメラを作成
     const camera = new ArcRotateCamera(
       "camera",
       -Math.PI / 2,
       Math.PI / 2.5,
-      10,
+      20,
+      // new Vector3(0, 2, -8),
       new Vector3(0, 0, 0),
       scene,
     );
@@ -103,15 +177,12 @@ export const Village = () => {
     // ユーザからの入力でカメラをコントロールするため、カメラをキャンバスにアタッチ
     camera.attachControl(canvas, true);
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
     const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    buildGround();
-    buildBox();
-    buildRoof();
+    buildDwellings();
 
     const bgm = new Sound("backgroundMusic", "/morning.ogg", scene, null, {
       loop: true,
